de3a53927c37796de02f894c5449d3db2537927f
TomyQB <montialvo@gmail.com>
2021-10-21 19:25:27 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { materialModule } from './core/material.module';
 import { LoadModule } from './modules/load/load.module';
 import { LoadComponent } from './modules/load/views/load.component';
 import { SearchModule } from './modules/search/search.module';
 import { SearchComponent } from './modules/search/views/search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LoadModule,
-    materialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
